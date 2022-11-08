ec30a28ba003d3f43c5b9c330f12ba9fb1690fa4
Gevorg Alaverdyan <galaverdyan@genetec.com>
2022-11-08 17:26:52 -0500

    sign in, next:error handling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
 
 function Profile() {
-  return (
-    <div>Profile</div>
-    
-  )
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+  useEffect(() => {
+    console.log(auth.currentUser)
+    setUser(auth.currentUser)
+  }, []);
+
+  return user ? <h1>{user.email}</h1> : <h1>Please Sign UP/IN</h1>
 }
 
-export default Profile
+export default Profile;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -13,12 +14,33 @@ function SignIn() {
   const navigate = useNavigate();
 
   const onChange = (e) => {
-      setFormData((prevState)=>({
-        ...prevState,
-        [e.target.id]: e.target.value,
-      }))
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate('/');
+      }
+
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className='pageContainer'>
@@ -26,7 +48,7 @@ function SignIn() {
           <p className='pageHeader'>Welcome Back!</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type='email'
             className='emailInput'
